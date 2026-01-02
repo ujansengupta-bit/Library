49804e03b1fe99f3e9f4de19e68af1c9e240f2ea
Ujan Sengupta <ujansenguptaofficial@gmail.com>
2026-01-02 20:56:42 +0530

    first half

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+
+let book = document.querySelector("#add")
+
+book.addEventListener("click", function (){
+    let new_book = document.querySelector(".pop_up");
+    new_book.style.display = "block";
+})
