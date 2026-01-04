c5ab989195f1a3eb7815da03176f5cad971434d5
Ujan Sengupta <ujansenguptaofficial@gmail.com>
2026-01-04 21:36:45 +0530

    worked on submit and read buttons

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, color) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = color;
 }
 
 function addBookToLibrary() {
   // take params, create a book then store it in the array
+  let new_book = document.querySelector(".pop_up");
+  let title_input = document.querySelector(".title").value;
+  let author_input = document.querySelector(".author").value;
+  let pages_input = document.querySelector(".pages").value;
+  let read_input = document.querySelector(".read");
+  
+  let bgcolor = read_input.style.backgroundColor;
+  let add = new Book(title_input, author_input, pages_input, bgcolor); 
+  new_book.style.display = "none";
+  title_input.value = "";
+  author_input.value = "";
+  pages_input.value = "";
 }
 
 
 let book = document.querySelector("#add")
-
 book.addEventListener("click", function (){
     let new_book = document.querySelector(".pop_up");
     new_book.style.display = "block";
+})
+let read_input = document.querySelector(".read");
+read_input.addEventListener("click", function(){
+  let bgcolor = read_input.style.backgroundColor;
+  if(bgcolor === "red"){
+    read_input.style.backgroundColor = "green";
+    read_input.textContent = "Read";
+  }
+  else{
+    read_input.style.backgroundColor = "red";
+    read_input.textContent = "Not Read";
+  }
+})
+
+let enter = document.querySelector(".submitting");
+enter.addEventListener("submit", function(){
+  event.preventDefault();
+  addBookToLibrary();
 })
